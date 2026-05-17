13830bd0b93b47058fc69661588aa5c0a67a43e4
yarogono <70641418+yarogono@users.noreply.github.com>
2026-05-17 11:25:44 +0900

    fix 고유가 피해지원금 지도 안나오는 오류

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
           strategy="beforeInteractive"
         />
       </head>
